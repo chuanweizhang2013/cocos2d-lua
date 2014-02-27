5edcadbbf855d22ca42da3bf426ac58378cea35b
James Chen <jianhua.chen@cocos2d-x.org>
2014-02-27 13:09:43 +0800

    Updates travis-script.

--- tools/tolua/genbindings.py
+++ tools/tolua/genbindings.py
@@ -91,7 +91,7 @@ def main():
         sys.exit(1)
 
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-    cocos_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '../lib/cocos2d-x'))
+    cocos_root = os.path.abspath(project_root, 'frameworks/lua-bindings/cocos2d-x'))
     cxx_generator_root = os.path.abspath(os.path.join(project_root, 'tools/bindings-generator'))
 
     # save config to file
