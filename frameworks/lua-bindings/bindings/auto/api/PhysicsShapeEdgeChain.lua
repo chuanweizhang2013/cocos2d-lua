b58b376010fddcc059c20f382be6be587b461a28
CocosRobot <redmine@cocos2d-x.org>
2014-03-03 06:31:59 +0000

    [AUTO] : updating luabinding automatically

--- frameworks/lua-bindings/bindings/auto/api/PhysicsShapeEdgeChain.lua
+++ frameworks/lua-bindings/bindings/auto/api/PhysicsShapeEdgeChain.lua
@@ -6,13 +6,4 @@
 -- @param self
 -- @return int#int ret (return value: int)
         
---------------------------------
--- @function [parent=#PhysicsShapeEdgeChain] create 
--- @param self
--- @param #point_table point
--- @param #int int
--- @param #cc.PhysicsMaterial physicsmaterial
--- @param #float float
--- @return PhysicsShapeEdgeChain#PhysicsShapeEdgeChain ret (return value: cc.PhysicsShapeEdgeChain)
-        
 return nil
