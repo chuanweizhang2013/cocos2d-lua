4ba8b2afedb4345a0c96a91e11bfa946740c6e4c
CocosRobot <redmine@cocos2d-x.org>
2014-03-05 09:57:45 +0000

    [AUTO] : updating luabinding automatically

--- frameworks/lua-bindings/bindings/auto/api/EventListenerTouchOneByOne.lua
+++ frameworks/lua-bindings/bindings/auto/api/EventListenerTouchOneByOne.lua
@@ -2,6 +2,11 @@
 --------------------------------
 -- @module EventListenerTouchOneByOne
 --------------------------------
+-- @function [parent=#EventListenerTouchOneByOne] isSwallowTouches 
+-- @param self
+-- @return bool#bool ret (return value: bool)
+        
+--------------------------------
 -- @function [parent=#EventListenerTouchOneByOne] setSwallowTouches 
 -- @param self
 -- @param #bool bool
