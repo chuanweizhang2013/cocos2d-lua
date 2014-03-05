d0fbd0c68a5abe0e19eb9c83a78b4f38f0392028
lihex <hexuan.li@chukong-inc.com>
2014-03-05 17:01:51 +0800

    remove mess code

--- templates/lua-template-default/frameworks/runtime-src/proj.win32/Runtime_win32.cpp
+++ templates/lua-template-default/frameworks/runtime-src/proj.win32/Runtime_win32.cpp
@@ -44,7 +44,6 @@ string getIPAddress()
 			if((hostinfo = gethostbyname(name)) != NULL)   
 			{ 
 				ip = inet_ntoa (*(struct in_addr *)*hostinfo->h_addr_list);
-				//printf(" IPµØÖ·: %s/n",ip);
 			}   
 		}   
 		WSACleanup( );
